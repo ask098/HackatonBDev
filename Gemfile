223e95be3e7c49dcb08ed76f374c48ac3306f8d8
ISAAC FLOREZ JIMENEZ <motato1456@gmail.com>
2020-02-22 18:02:00 -0500

    Commit

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 # gem to autentication
-gem 'devise'
+# gem 'devise'
 # adding bootstrap to app
 gem 'bootstrap'
 
--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -5,21 +5,21 @@ class AthletesController  < ApplicationController
         #
     end
     
-    # def new
-    #     @athlete = Athlete.new
-    # end
+     def new
+         @athlete = Athlete.new
+     end
     
-    # def create
-    #     @athlete = Athlete.new(athlete_params)
-    #     @athlete.user_id = current_user.id
-    #     if @athlete.save
-    #         redirect_to @athlete
-    #         flash[:notice] = "athlete created!"
-    #     else
-    #         redirect_back(fallback_location: root_path)
-    #         flash[:alart] = "athlete creation failed"
-    #     end    
-    # end
+     def create
+         @athlete = Athlete.new(athlete_params)
+         @athlete.user_id = current_user.id
+         if @athlete.save
+             redirect_to @athlete
+             flash[:notice] = "athlete created!"
+         else
+             redirect_back(fallback_location: root_path)
+             flash[:alart] = "athlete creation failed"
+         end    
+     end
     
     def show
         @athlete = Athlete.find(params[:id])
